83f72e7d00e221b946688542065c0ab7ec2419d8
HandrewOltenish <148466747+HandrewOltenish@users.noreply.github.com>
2024-05-17 16:34:31 +0200

    Renamed functions to handle Libft conflicts

--- Core Curriculum/get_next_line/gnl_mandatory/get_next_line.h
+++ Core Curriculum/get_next_line/gnl_mandatory/get_next_line.h
@@ -25,10 +25,10 @@
 char	*ft_next(char *buffer);
 char	*ft_line(char *buffer);
 char	*ft_get(int fd, char *buffer);
-char	*ft_strjoin(char *s1, char *s2);
+char	*ft_gnl_strjoin(char *s1, char *s2);
 char	*get_next_line(int fd);
-char	*ft_strchr(const char *s, int c);
-void	ft_bzero(void *s, size_t n);
-void	*ft_calloc(size_t n, size_t size);
-size_t	ft_strlen(const char *s);
+char	*ft_gnl_strchr(const char *s, int c);
+void	ft_gnl_bzero(void *s, size_t n);
+void	*ft_gnl_calloc(size_t n, size_t size);
+size_t	ft_gnl_strlen(const char *s);
 #endif
