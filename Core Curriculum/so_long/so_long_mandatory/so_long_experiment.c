ff5f5e24b06b6117cedaad146c27db1de8fefc4e
HandrewOltenish <148466747+HandrewOltenish@users.noreply.github.com>
2024-11-10 20:39:18 +0100

    Added map handling function

--- Core Curriculum/so_long/so_long_mandatory/so_long_experiment.c
+++ Core Curriculum/so_long/so_long_mandatory/so_long_experiment.c
@@ -6,7 +6,7 @@
 /*   By: aolteanu <aolteanu.student@42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/18 13:15:51 by aolteanu          #+#    #+#             */
-/*   Updated: 2024/11/07 19:51:27 by aolteanu         ###   ########.fr       */
+/*   Updated: 2024/11/10 20:33:32 by aolteanu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,6 +63,47 @@ void	key_movement(mlx_key_data_t keydata, void	*param)
 	detect_collision(keydata, img);
 }
 
+int char_check(char c)
+{
+	if (c == GAME_PLAYER
+		|| c == GAME_EXIT
+		|| c == GAME_COLLECTIBLE
+		|| c == GAME_WALL)
+		return (1);
+	return (0);
+}
+
+char *map_handle_experiment(int stringscount, t_game_map	*map)
+{
+	int size;
+
+	size = 0;
+	map->collectible_count = 0;
+	map->player_count = 0;
+	map->exit_count = 0;
+	while (map->copy[map->map_height])
+	{
+		while (map->copy[0] || map->copy[stringscount])
+		{
+			if (map->copy[map->map_height][map->map_width] != GAME_WALL)
+				return("Invalid map: Outer perimeter invalid.");
+			map->map_width++;
+		}
+		while (map->copy[map->map_height][map->map_width])
+		{
+			if (!char_check(map->copy[map->map_height][map->map_width]))
+				return("Invalid map: Illegal character inside map.");
+			map->map_width++;
+		}
+		if (size == 0)
+			size = map->map_width;
+		else if (size != map->map_width)
+			return("Invalid map: Different line size");
+		map->map_height++;
+	}
+	return ("Valid Map! Enjoy the game!");
+}
+
 void path_handle_experiment(t_game_map	*map, int j, int i) 
 {
 	if (!map->collectible_count && !map->exit_count && !map->player_count)
@@ -71,53 +112,96 @@ void path_handle_experiment(t_game_map	*map, int j, int i)
 	{
 		puts("Invalid map.");
 		return ;
+	// player = mlx_load_png("./char.png");
 	}
 	if (map->map[j][i] == 'C')
 		map->collectible_count--;
 	if (map->map[j][i] == 'E')
 		map->exit_count--;
 	if (map->map[j][i] == 'P')
-	map->map[j][i] == 'X';
-	path_handle_experiment(map, j, i - 1);
-	path_handle_experiment(map, j, i + 1);
-	path_handle_experiment(map, j - 1, i);
-	path_handle_experiment(map, j + 1, i);
-	
+	map->map[j][i] = 'X';
+	if (map->map[j][i - 1])
+		path_handle_experiment(map, j, i - 1);
+	if (map->map[j][i + 1])
+		path_handle_experiment(map, j, i + 1);
+	if (map->map[j - 1][i])
+		path_handle_experiment(map, j - 1, i);
+	if (map->map[j + 1][i])
+		path_handle_experiment(map, j + 1, i);
 }
 
 int	ft_countstrings(int fd)
 {
 	int count;
+	char	*str;
 
 	count = 0;
+	str = "";
 	while (fd)
 	{
-		get_next_line(fd);
+		str = get_next_line(fd);
+		if (!str)
+			return (count);
 		count++;
 	}
 	return (count);
 }
 
+void copy_map(int fd,char** map, int strings_count)
+{
+	int j;
+
+	j = 0;
+	while (j <= strings_count)
+	{
+		map[j] = get_next_line(fd);
+		printf("%s\n", map[j]);
+		j++;
+	}
+	close(fd);
+}
+
 // Split the functions. One function is doing too many things to accurately track in the future!
 int main(void)
 {
-	mlx_t	*mlx = mlx_init(WIDTH, HEIGHT, "Project Escape Schengen", true);
-	mlx_texture_t *player;
-	mlx_image_t	*player_img;
+	// mlx_t	*mlx = mlx_init(WIDTH, HEIGHT, "Project Escape Schengen", true);
+	// mlx_texture_t *player;
+	// mlx_image_t	*player_img;
+	t_game_map *map;
+	int fd;
+	int strings_count;
 
-	if (!mlx)
-		printf("Error: Could not initialize mlx");
-	player = mlx_load_png("./char.png");
-	if (!player)
-		printf("Error: Could not load player texture");
-	player_img = mlx_texture_to_image(mlx, player);
-	if (!player_img)
-		printf("Error: Could not create player image");
-	if (mlx_image_to_window(mlx, player_img, WIDTH -200, HEIGHT - 200) < 0)
-		printf("Error: Could not draw window");
-	mlx_key_hook(mlx, (mlx_keyfunc)key_movement, player_img);
-	mlx_loop(mlx);
-	mlx_delete_image(mlx, player_img);
-	mlx_terminate(mlx);
+	fd = open(MAP_PATH, O_RDONLY);
+	if (fd < 0)
+		puts("Error: Could not open file. Check if file exists");
+	map = (t_game_map *)malloc(sizeof(t_game_map));
+	if (!map)
+		puts("Error: Failed to allocate memory for struct");
+	strings_count = ft_countstrings(fd);
+	close(fd);
+	fd = open(MAP_PATH, O_RDONLY);
+	if (fd < 0)
+		puts("Error: Could not open file. Check if file exists");
+	map->copy = (char **)malloc((strings_count + 1) * sizeof(char *));
+		if (!map->copy)
+		puts("Error: Failed to allocate memory for copy");
+	copy_map(fd, map->copy, strings_count);
+	// if (!mlx)
+	// 	puts("Error: Could not initialize mlx");
+	// player = mlx_load_png("./char.png");
+	// if (!player)
+	// 	puts("Error: Could not load player texture");
+	// player_img = mlx_texture_to_image(mlx, player);
+	// if (!player_img)
+	// 	puts("Error: Could not create player image");
+	// if (mlx_image_to_window(mlx, player_img, WIDTH -200, HEIGHT - 200) < 0)
+	// 	puts("Error: Could not draw window");
+	// mlx_key_hook(mlx, (mlx_keyfunc)key_movement, player_img);
+	// mlx_loop(mlx);
+	printf("%s", map_handle_experiment(strings_count, map));
+	// path_handle_experiment(map, 0, 0);
+	// mlx_delete_image(mlx, player_img);
+	// mlx_terminate(mlx);
+	// free(map);
 	return (EXIT_SUCCESS);
 }
